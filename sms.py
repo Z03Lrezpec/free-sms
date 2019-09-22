e55890d44fb73ab4dc5399e3faeabcf2deb75050
bellattckr <54947269+bellattckr@users.noreply.github.com>
2019-09-22 08:58:13 +0800

    Update sms.py

--- sms.py
+++ sms.py
@@ -1,11 +1,35 @@
-# MR.K7C8NG
+# MR.BELL
 
 from requests import Session
 import re, sys
 s = Session()
 
 try:
-	print("\n\n * SMS Gratis by MR.K7C8NG\n * Gunakan kode negara (ex: 628xxxxx)\n")
+	print(''' \033[96m	
+         _,met$$$$$gg.           
+      ,g$$$$$$$$$$$$$$$P.        
+    ,g$$P""       """Y$$.".      
+   ,$$P'              `$$$.      
+  ',$$P       ,ggs.     `$$b:    
+  `d$$'     ,$P"'   .    $$$     
+   $$P      d$'     ,    $$P     
+   $$:      $$.   -    ,d$$'     
+   $$\;      Y$b._   _,d$P'      
+   Y$$.    `.`"Y$$$$P"'          
+   `$$b      "-.__
+    `Y$$
+     `Y$$.
+       `$$b.
+         `Y$$b.
+            `"Y$b._
+                `""""
+|Version   : V.1(Beta)      |
+|Team      : THE NEXT LEVEL |
+|Create    : Mr.Bell        |
+| Free SMS FOR ALL OPERATOR |
+############################## \n \033[91m''')
+
+	
 	no = int(input(" Nomor : "))
 	msg = input(" Pesan : ")
 except:
